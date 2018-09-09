e37d1f3e5377c8766024fbc02e185726307c1e3a
D3n0Duz <denoduz@gmail.com>
2018-09-09 02:22:48 -0400

    add foca

---
+++ information_gathering/foca/Foca.py
@@ -0,0 +1,30 @@
+'''
+    #####################################
+    # What is FOCA
+    ######################################
+
+    # https://www.elevenpaths.com/labstools/foca/index.html
+    # FOCA (Fingerprinting Organizations with Collected Archives) is a tool used mainly to find metadata and hidden information in the documents its scans
+
+    ######################################
+    # What it does
+    ######################################
+
+    # Search all documents in a domain
+    # Download, analyze and create metadata out of the documents
+    # Shows Users
+    # Shows Folders
+    # Shows Printers
+    # Shows Software
+    # Shows E-mails
+    # Shows OS
+    # Shows Passwords
+    # Shows Servers
+
+    ######################################
+    # How to use it
+    ######################################
+
+    # Since it's a GUI, it's pretty straight forward (Entering project name, domain and date)
+
+'''
