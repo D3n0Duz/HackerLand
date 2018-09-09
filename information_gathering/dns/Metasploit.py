7a4397d54e85b918fdd8e3bc65edc931c7eb2a7f
D3n0Duz <denoduz@gmail.com>
2018-09-09 02:14:22 -0400

    add metasploit dns

---
+++ information_gathering/dns/Metasploit.py
@@ -0,0 +1,24 @@
+'''
+    #####################################
+    # Covered modules
+    ######################################
+
+    # auxiliary/gather/dns_bruteforce
+    # auxiliary/gather/dns_cache_scrapper
+    # auxiliary/gather/dns_info
+    # auxiliary/gather/dns_reverse_lookup
+    # auxiliary/gather/dns_srv_enum
+
+
+    ######################################
+    # How to use metasploit (exemple)
+    ######################################
+
+    # Make sure that you have installed the tool
+    # Open terminal (Ctrl + Alt + t)
+    # msfconsole
+    # use auxiliary/gather/dns_reverse_lookup
+    # set RANGE <enter the IP of your target>
+    # run
+
+'''
