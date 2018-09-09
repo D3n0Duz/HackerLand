c3f0e7a27e146db67d485678e47e49afb9acdbe1
D3n0Duz <denoduz@gmail.com>
2018-09-09 14:29:36 -0400

    add metasploit install instructions

--- information_gathering/dns/Metasploit.py
+++ information_gathering/dns/Metasploit.py
@@ -1,4 +1,10 @@
 '''
+    ######################################
+    # Installation
+    ######################################
+
+    # wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run && wget https://downloads.metasploit.com/data/releases/metasploit-latest-linux-x64-installer.run.sha1 && echo $(cat metasploit-latest-linux-x64-installer.run.sha1)'  'metasploit-latest-linux-x64-installer.run > metasploit-latest-linux-x64-installer.run.sha1 && shasum -c metasploit-latest-linux-x64-installer.run.sha1 && chmod +x ./metasploit-latest-linux-x64-installer.run && sudo ./metasploit-latest-linux-x64-installer.run
+
     #####################################
     # Covered modules
     ######################################
@@ -20,5 +26,6 @@
     # use auxiliary/gather/dns_reverse_lookup
     # set RANGE <enter the IP of your target>
     # run
+
 
 '''
